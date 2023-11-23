ae721a0f685073aaf1194272bec0e2f31d6b34db
Mihai Childesco <mihai.childesco@gmail.com>
2023-11-23 23:35:35 +0200

    Add modal

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 // Query Selectors
 const habits = document.querySelectorAll(".habit-btn");
 const themeBtn = document.querySelector("#theme");
+const modalContainer = document.querySelector(".modal-container");
+const createHabitBtn = document.querySelector(".new-habit__add");
+const newHabitTitle = document.querySelector("#title");
+const icons = document.querySelectorAll(".icon");
+const addBtn = document.getElementById("#add");
+const cancelBtn = document.querySelector("#cancel");
 
 // FUNCTIONS
 
@@ -22,6 +28,22 @@ const ui = {
       ? storage.saveTheme("dark")
       : storage.saveTheme("light");
   },
+  openModal() {
+    modalContainer.classList.add("active");
+    modalContainer.setAttribute("aria-hidden", "false");
+    newHabitTitle.focus();
+  },
+  closeModal() {
+    modalContainer.classList.remove("active");
+    modalContainer.setAttribute("aria-hidden", "true");
+    newHabitTitle.value = "";
+    ui.removeSelectedFromIcon();
+  },
+  removeSelectedFromIcon() {
+    icons.forEach((icon) => {
+      icon.classList.remove("selected");
+    });
+  },
 };
 
 // EVENT LISTENERS
@@ -34,8 +56,21 @@ window.addEventListener("DOMContentLoaded", () => {
 });
 
 // EVENT: theme button
-
 themeBtn.addEventListener("click", ui.theme);
+
+// EVENT: add habit btn
+createHabitBtn.addEventListener("click", ui.openModal);
+
+// EVENT: close modal
+cancelBtn.addEventListener("click", ui.closeModal);
+
+// EVENT: selected icon
+icons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    ui.removeSelectedFromIcon();
+    icon.classList.add("selected");
+  });
+});
 
 habits.forEach((habit) => {
   habit.addEventListener("click", () => {
